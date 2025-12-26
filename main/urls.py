8ef500bd112ded00142457aacea4233a2b496bb3
Elbek <meliqoziyevelbek7@gmail.com>
2025-12-26 05:29:46 +0500

    second change

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,11 @@ from .views import *
 urlpatterns = [
     path('api/books/', BooksApi.as_view(), name='book'),
     path('api/books/<int:pk>/', One_Book.as_view(), name='book_id'),
+    path('api/hello/', hello, name='hello'),
+    path('api/greet/', greet, name='greet'),
+    path('api/echo/', echo, name='echo'),
+    path('api/check-age/', check_age, name='age'),
+    path('api/register/', register, name='register'),
+    path('api/square/<int:num>/', square, name="kv"),
+
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from .models import *
 
 
@@ -25,14 +26,13 @@ class BooksApi(APIView):
         for book in books:
             data.append(dict_to_json(book))
         return Response(data=data)
-    
-    
+        
 
 class One_Book(APIView):
     def get(self, request, pk):
         try:
             book = Book.objects.get(pk=pk)
-        except:
+        except Book.DoesNotExist:
             return Response({"message": "Ma'lumot topilmadi"},status=404)
         return Response(data=dict_to_json(book))
     
@@ -40,16 +40,26 @@ class One_Book(APIView):
         try:
             book = Book.objects.get(pk=pk)
             book.delete()
-        except:
+        except Book.DoesNotExist:
             return Response({"message": "Ma'lumot topilmadi"},status=404)
         
         return Response(data=dict_to_json(book))
     
-    
+    def put(self, request: Request, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response({"message": "Ma'lumot topilmadi"},status=404)
+        
+        data = request.data
+        book.name = data.get("name")
+        book.author = data.get("author")
+        book.year = data.get("year")
+        book.price = data.get("price")
 
-
-
-
+        book.save()
+        
+        return Response(data=dict_to_json(book), status=200)
 
 
 def dict_to_json(book):
@@ -57,5 +67,43 @@ def dict_to_json(book):
         "id":book.id,
         "name":book.name,
         "author":book.author,
-        "year":book.year
+        "year":book.year,
+        "price":book.price
     }
+
+
+@api_view(['GET'])
+def hello(request: Request):
+    return Response({"message":"Hello,API!"})
+
+@api_view(['GET'])
+def greet(request: Request):
+    name = request.query_params.get('name')
+    return Response({"message":f"Hello, {name}"})
+
+@api_view(['POST'])
+def echo(request: Request):
+    data = request.data.get("text")
+    return Response({"matn":data})
+
+@api_view(['POST'])
+def check_age(request: Request):
+    data = request.data.get("age")
+    if data < 18:
+        return Response({"message":"Access denied"})
+    else:
+        return Response({"message":"Access granted"})
+    
+@api_view(['POST'])
+def register(request: Request):
+    try:
+        data = request.data['username']
+    except:
+        return Response(status=400)
+    return Response({"message":f"{data} registered"}, status=201)
+
+@api_view(['GET'])
+def square(request: Request, num):
+    data = num**2
+    return Response({"number":num, "square":data})
+    
